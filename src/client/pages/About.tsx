cd9d001800ddd1d4d599afab9744a0e32248f0a9
Daniil <temnay2@icloud.com>
2025-07-20 23:51:19 +0300

    test changes

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Anastasia Papchenko
+            Author: Anastasia Padpchenko
           </p>
         </div>
       </div>
